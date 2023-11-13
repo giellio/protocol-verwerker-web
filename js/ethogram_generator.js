31854f0624eb1d23ddd75b0e9ed1c6ad12c47cf3
Giellio <gielmichael@hotmail.com>
2023-11-13 17:29:53 +0100

    Fix error

--- js/ethogram_generator.js
+++ js/ethogram_generator.js
@@ -1,4 +1,4 @@
-async function processFile (file) {
+async function processFile(file) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
     reader.readAsText(file)
@@ -23,47 +23,48 @@ async function processFile (file) {
   })
 }
 
-async function ethogram (files, only = false) {
+async function ethogram(files, only = false) {
   const elementen = []
 
   for (const file of files) {
     const fileElementen = await processFile(file)
     for (let x of fileElementen) {
-          x = x.trim()
-          if (elementen.indexOf(x) === -1) {
-            elementen.push(x)
-          }
-  }
+      x = x.trim()
+      if (elementen.indexOf(x) === -1) {
+        elementen.push(x)
+      }
+    }
 
-  console.log('elementen')
-  console.log(elementen)
+    console.log('elementen')
+    console.log(elementen)
 
-  const ethogram = [['Afkorting', 'Gedrag', 'Beschrijving']]
-  for (let index = 0; index < elementen.length; index++) {
-    const element = elementen[index]
-    ethogram.push([element, '', ''])
-  }
+    const ethogram = [['Afkorting', 'Gedrag', 'Beschrijving']]
+    for (let index = 0; index < elementen.length; index++) {
+      const element = elementen[index]
+      ethogram.push([element, '', ''])
+    }
 
-  console.log('ethogram')
-  console.table(ethogram)
+    console.log('ethogram')
+    console.table(ethogram)
 
-  let csvContent = ''
+    let csvContent = ''
 
-  ethogram.forEach(function (rowArray) {
-    const row = rowArray.join(',')
-    csvContent += row + '\r\n'
-  })
+    ethogram.forEach(function (rowArray) {
+      const row = rowArray.join(',')
+      csvContent += row + '\r\n'
+    })
 
-  if (only) {
-    const encodedUri = encodeURI('data:text/csv;charset=utf-8,' + csvContent)
-    const link = document.createElement('a')
-    link.setAttribute('href', encodedUri)
-    link.setAttribute('download', 'ethogram.csv')
-    document.body.appendChild(link) // Nodig voor Firefox.
+    if (only) {
+      const encodedUri = encodeURI('data:text/csv;charset=utf-8,' + csvContent)
+      const link = document.createElement('a')
+      link.setAttribute('href', encodedUri)
+      link.setAttribute('download', 'ethogram.csv')
+      document.body.appendChild(link) // Nodig voor Firefox.
 
-    link.click() // Hiermee wordt het gegevensbestand gedownload met de naam "sequentietabel.csv".
-  } else {
-    console.log('return')
-    return csvContent
+      link.click() // Hiermee wordt het gegevensbestand gedownload met de naam "sequentietabel.csv".
+    } else {
+      console.log('return')
+      return csvContent
+    }
   }
 }
